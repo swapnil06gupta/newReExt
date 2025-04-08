e699ba66c5daf5f77206c2dc034b11191402ca0f
Mohan Chhalotre <mohan@timecounts.app>
2025-04-08 20:47:37 +0530

    change configs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
-import reext from "./node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js";
+// import reext from "./node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js";
+import reext from "./vite-plugin-reext.js";
 export default defineConfig({
   plugins: [reext()],
 });
