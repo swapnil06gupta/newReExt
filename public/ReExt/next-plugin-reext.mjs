9e24260cf8bf4dd398a43780bfd818aa6cd37cd2
Mohan Chhalotre <mohan@timecounts.app>
2025-04-08 21:23:16 +0530

    change configs

--- public/ReExt/next-plugin-reext.mjs
+++ public/ReExt/next-plugin-reext.mjs
@@ -3,6 +3,12 @@ const reextPlugin = (nextConfig = {}) => {
     ...nextConfig,
     webpack(config, options) {
       // Define the global variable
+      const reextKey =
+        "bzJOOXhIaHJzTlZMZG1wXzg3dFR6N3NlQlVNakwwMGNoRDlwUWJobFJTbS45bFRPeWN6TjJVRE4zRWpPaUFIZwxKQ0xpY0dkeFpuZDVFVFp3Qm5NejBHT29KemN0Tm5iNkpXY3FGRE1mUldhc0ppT2lJV2R6SnllLjlKaU4xSXpVSUppT2ljR2JoSnll";
+      config.define = {
+        ...config.define,
+        __IS_REEXT_RUNNING__: JSON.stringify(reextKey),
+      };
       config.plugins.push(
         new options.webpack.DefinePlugin({
           __IS_REEXT_RUNNING__: JSON.stringify("true"),
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 // import reext from "./node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js";
-import reext from "../reext-demo/public/ReExt/vite-plugin-reext.js";
+import reextPlugin from "./public/ReExt/next-plugin-reext.mjs";
 export default defineConfig({
-  plugins: [reext()],
+  plugins: [reextPlugin()],
 });
